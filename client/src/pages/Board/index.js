90395718760396a8e844daa682834da02659a698
66maer <505670805@qq.com>
2024-12-27 18:03:59 +0800

    更新Board页面以使用新的BoardContent组件，添加团队信息展示和样式优化，新增团队标签样式

--- client/src/pages/Board/index.js
+++ client/src/pages/Board/index.js
@@ -1,22 +1,14 @@
 import React, { useState, useEffect } from "react";
-import {
-  Flex,
-  Layout,
-  Menu,
-  Space,
-  Button,
-  Avatar,
-  Spin,
-  Typography,
-} from "antd";
+import { Flex, Layout, Menu, Space, Button, Avatar, Spin, Typography, Tag } from "antd";
 import { request } from "@/utils/request";
 import SlotCard from "@/components/SlotCard";
 import DateTag from "../../components/DateTag";
 import "./index.scss";
 import SlotPanel from "../../components/SlotPanel";
+import { CompassOutlined, ClockCircleOutlined } from "@ant-design/icons";
 
 const { Header, Content, Footer, Sider } = Layout;
-const { Title } = Typography;
+const { Title, Paragraph } = Typography;
 
 const fetchTeamList = async () => {
   const res = await request.get("/team/list");
@@ -27,9 +19,64 @@ const fetchTeamList = async () => {
   return res.data;
 };
 
-const BoardContent = (team) => {
-  const { id, title, teamTime, dungeons, role, notice } = team;
+const teamList = [
+  {
+    id: "1",
+    title: "12月27日 第一车",
+    teamTime: "20:00",
+    dungeons: "英雄一之窟",
+    rule: "规则",
+    notice: "这是公告，",
+    bookXuanjing: false,
+    bookYuntie: true,
+    isLock: true,
+    crateTime: "2021-12-27T12:00:00",
+    updateTime: "2021-12-27T12:00:00",
+  },
+];
+
+const BoardContent = ({ team = {} }) => {
+  const { id, title, teamTime, dungeons, rule, notice } = team;
   const { bookXuanjing, bookYuntie, isLock, crateTime, updateTime } = team;
+  const [expanded, setExpanded] = useState(false);
+
+  return (
+    <div style={{ display: "flex", flexDirection: "column", width: "1000px" }}>
+      <Title level={2} style={{ margin: 0 }}>
+        {isLock && <Avatar size={32} shape="square" src="/lock.svg" style={{ marginTop: -6 }} />}
+        {title}
+      </Title>
+      <Paragraph>
+        <pre>
+          <Tag icon={<CompassOutlined />} className="team-tag" color="geekblue">
+            {dungeons}
+          </Tag>
+          <Tag icon={<ClockCircleOutlined />} className="team-tag" color="cyan">
+            {teamTime}
+          </Tag>
+          <Tag className="team-tag" icon={<img src={"玄晶.png"} alt="玄晶" />} color={bookXuanjing ? "#f50" : "#5a0"}>
+            {bookXuanjing ? "大铁已包" : "大铁尚在"}
+          </Tag>
+          <Tag className="team-tag" icon={<img src={"陨铁.png"} alt="陨铁" />} color={bookYuntie ? "#f50" : "#5a0"}>
+            {bookYuntie ? "小铁已包" : "小铁尚在"}
+          </Tag>
+          <blockquote>
+            <Paragraph
+              ellipsis={{
+                rows: 3,
+                expandable: "collapsible",
+                expanded,
+                onExpand: (_, info) => setExpanded(info.expanded),
+              }}
+            >
+              {notice.repeat(200)}
+            </Paragraph>
+          </blockquote>
+        </pre>
+      </Paragraph>
+      <SlotPanel />
+    </div>
+  );
 };
 
 const Board = () => {
@@ -85,9 +132,7 @@ const Board = () => {
       label: (
         <Space>
           12月28日
-          <DateTag
-            date={new Date(new Date().getTime() + 24 * 60 * 60 * 1000)}
-          />
+          <DateTag date={new Date(new Date().getTime() + -3 * 24 * 60 * 60 * 1000)} />
         </Space>
       ),
       children: [
@@ -134,16 +179,7 @@ const Board = () => {
           overflow: "auto", // 添加此行
         }}
       >
-        <div style={{ display: "flex", flexDirection: "column" }}>
-          <Title>12月27日 第二车</Title>
-          <Space>
-            发车时间: 20:00
-            <Avatar>玄晶</Avatar>
-            <Avatar>陨铁</Avatar>
-          </Space>
-          <br />
-          <SlotPanel />
-        </div>
+        <BoardContent team={teamList[0]} />
       </Content>
     </Layout>
   );
