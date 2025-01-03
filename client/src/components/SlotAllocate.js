7eddfef12f76724c230ae1c7b9f573e75ff9cd13
66maer <505670805@qq.com>
2025-01-03 17:57:24 +0800

    重构SlotAllocate函数，优化算法逻辑，简化代码结构，增强可读性

--- client/src/components/SlotAllocate.js
+++ client/src/components/SlotAllocate.js
@@ -1,12 +1,3 @@
-// 帮我写一个算法, 实现一个函数 SlotAllocate(teamRules, teamLogs)
-// 函数接收两个参数, teamRules 是一个长度25的数组表示团队报名的规则(我们成为25个坑), 每个元素是一个对象, 对象包含两个属性, allow_xinfa_list
-// teamLogs 也是一个数组表示报名的操作, 每个元素是一个对象, 包含一条操作日志的json logJson, 一个操作类型 type(报名，取消报名，代报名，取消代报名，管理员指定，管理员取消)
-// 函数需要返回两个数据, 一个长度25的数组表示每个坑位的报名情况teamSlots, 每个元素是一个对象, 表示具体的报名情况(称为填坑),
-// 包含的字段有: 报名日志的id, 报名人id, 姓名username, 游戏角色名characterName, xinfa角色心法，用于匹配规则， 提交者id submit_user_id(存在代报名的情况), 取消人的id cancel_user_id(存在别人代我报名，我自己取消，或者管理员取消的情况)，报名类型sign(常规，代报名，管理员指定)
-// 还有需要返回一个处理后的报名日志描述，每个元素是一个字符串, 因为teamLogs是按照时间记录的对象，而这个数组需要对其处理，比如将报名又取消的情况合并，输出一条"xxx报名(被xxx取消)"的描述
-
-import { xinfaInfoTable } from "@/utils/xinfa";
-
 const SampleAllocate = (rules, slots, signupInfo, last = -1) => {
   for (let idx = last + 1; idx < rules.length; idx++) {
     const rule = rules[idx];
@@ -26,7 +17,7 @@ const SampleAllocate = (rules, slots, signupInfo, last = -1) => {
   return -1;
 };
 
-const dfs = (resSlots, rules, slots, signupList, candidateList) => {
+const DFS = (resSlots, rules, slots, signupList, candidateList) => {
   if (signupList.length == 0) {
     resSlots.splice(0, resSlots.length, ...slots);
     return true;
@@ -49,9 +40,7 @@ const dfs = (resSlots, rules, slots, signupList, candidateList) => {
       if (last == -1) {
         return false;
       }
-      if (
-        dfs(resSlots, rules, newSlots, signupList.slice(idx + 1), candidateList)
-      ) {
+      if (DFS(resSlots, rules, newSlots, signupList.slice(idx + 1), candidateList)) {
         return true;
       }
     }
@@ -60,11 +49,6 @@ const dfs = (resSlots, rules, slots, signupList, candidateList) => {
 };
 
 const SlotAllocate = (teamRules, signupList) => {
-  // const teamSlots = teamRules.map((rule) => ({
-  //   rule: rule,
-  //   member: null,
-  // }));
-
   const slotMemberList = Array(teamRules.length).fill(null);
   const candidateList = [];
   for (const [idx, signupInfo] of signupList.entries()) {
@@ -89,13 +73,7 @@ const SlotAllocate = (teamRules, signupList) => {
     const newSlotMemberList = Array(teamRules.length).fill(null);
     const tmpSlotMemberList = newSlotMemberList.slice();
 
-    const res = dfs(
-      newSlotMemberList,
-      teamRules,
-      tmpSlotMemberList,
-      signupList.slice(0, idx + 1),
-      candidateList
-    );
+    const res = DFS(newSlotMemberList, teamRules, tmpSlotMemberList, signupList.slice(0, idx + 1), candidateList);
     if (res) {
       slotMemberList.splice(0, slotMemberList.length, ...newSlotMemberList);
     } else {
@@ -103,7 +81,14 @@ const SlotAllocate = (teamRules, signupList) => {
     }
   }
 
-  console.log(slotMemberList, candidateList);
+  const teamSlots = slotMemberList.map((signupInfo, idx) => {
+    return {
+      rule: teamRules[idx],
+      member: signupInfo,
+    };
+  });
+
+  return [teamSlots, candidateList];
 };
 
 export default SlotAllocate;
