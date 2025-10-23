d51b9c858db4c327c2004305a9f08ccdadc24eb9
66maer <505670805@qq.com>
2025-10-23 22:33:23 +0800

    更新副本表，移除冗余条目并调整城池名称

--- client/src/utils/dungeons.js
+++ client/src/utils/dungeons.js
@@ -4,19 +4,19 @@ export const dungeonsTable = [
     label: "英雄太极宫",
   },
   {
-    value: "普通太极宫",
-    label: "普通太极宫",
-  },
-  {
     value: "空城殿·上",
     label: "空城殿·上",
   },
   {
-    value: "英雄一之窟",
-    label: "英雄一之窟",
+    value: "空城殿·下",
+    label: "空城殿·下",
   },
   {
-    value: "普通一之窟",
-    label: "普通一之窟",
+    value: "英雄弓月城",
+    label: "英雄弓月城",
+  },
+  {
+    value: "普通弓月城",
+    label: "普通弓月城",
   },
 ];
